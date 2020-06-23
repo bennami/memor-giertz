347cbd2b47333345362cff88ff9e81c8a092c300
bennami <benbenimane@hotmail.com>
2020-06-23 16:35:16 +0200

    duplicate arrae, shuffle it randomly, generate html, add data type to check what has been clicked

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const cards = [
     {
         name: 'qr',
         imgUrl: 'assets/img/monkey.png'
+    },
+    {
+        name: 'koen',
+        imgUrl: 'assets/img/koen.jpeg'
+    },
+    {
+        name: 'sicco',
+        imgUrl: 'assets/img/sicco.jpeg'
     }
 ]
 
@@ -21,29 +29,35 @@ allCardImgUrl = cards.concat(cards);
 //shuffle
 allCardImgUrl.sort(() => Math.random() - 0.5);
 
-
-
-
-
-
-
-//create imgs
-let arrCards = [];
-    allCardImgUrl.forEach(img=>{
+//generate htlm tags and add click event
+allCardImgUrl.forEach(img=>{
         //generate divs with imgs and append to div container
         let cardContainer = document.createElement('div');
         cardContainer.style.backgroundImage = 'Url("assets/img/becode.png")';
         let card = document.createElement('img');
         card.setAttribute('src', img.imgUrl);
+        card.setAttribute('data-id', img.name);
+        card.setAttribute('class',"card");
         cardContainer.appendChild(card);
         const canvas = document.querySelector(".mem-canvas");
         canvas.appendChild(cardContainer);
 
-        //flip card on click
-        card.addEventListener("click", ()=>{
-        card.style.opacity = '1';
-        });
+    //flip card on click
+    // have a counter to prevent clicking on more than 2 cards,
+    // push the 2 cards in array and do a check to see if the data matches
+    // if its no match flip em back
+    card.addEventListener("click", ()=>{
+
+       card.style.opacity ="1";
+       console.log(card);
+
     });
+});
+
+
+
+
+
 
 
 
